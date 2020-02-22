dfa7d5ed7d1d5287b5a519441c870f80bffb8558
Paweł Bedra <s21963@pjwstk.edu.pl>
2020-02-22 12:51:50 +0100

    pierwszy projekt

---
+++ java-basics_PB/src/pl/pjatk/Main.java
@@ -0,0 +1,93 @@
+package pl.pjatk;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int myFirstNumber = 35;
+    int mySecondNumber = 12;
+    int myThirdNumber = myFirstNumber + mySecondNumber;
+
+    int intMaxValue = Integer.MAX_VALUE;
+    int intMinValue = Integer.MIN_VALUE;
+
+    byte byteMaxValue = Byte.MAX_VALUE;
+    byte byteMinValue = Byte.MIN_VALUE;
+
+    short shortMaxValue = Short.MAX_VALUE;
+    short shortMinValue = Short.MIN_VALUE;
+
+    long longMaxValue = Long.MAX_VALUE;
+    long longMinValue = Long.MIN_VALUE;
+
+
+/*
+    System.out.println("Hello, PJATK");
+    System.out.println(myThirdNumber);
+
+    System.out.println("Integer max value = " + intMaxValue);
+    System.out.println("Integer min value = " + intMinValue);
+
+    System.out.println("Byte max value = " + byteMaxValue);
+    System.out.println("Byte min value = " + byteMinValue);
+
+    System.out.println("Short max value = " + shortMaxValue);
+    System.out.println("Short min value = " + shortMinValue);
+
+    System.out.println("Long max value = " + longMaxValue);
+    System.out.println("Long min value = " + longMinValue);
+*/
+
+/*    short bigShortValue = 32767;
+    int myTotal = (byteMinValue / 2);
+
+    short myNewShortValue = (short) myTotal;
+
+    System.out.println("My new casted value = " + myNewShortValue);
+
+    byte liczbaByte = 2;
+    short liczbaShort = 4;
+    int liczbaInt = 6;
+
+    long liczbaLong = 5000;
+    long wynik = liczbaLong + 10 * ((long)liczbaByte + (long)liczbaInt + (long)liczbaShort);
+
+    System.out.println("Wynik = " + wynik);
+*/
+/*
+    boolean isReady = true;
+    boolean isReady2 = false;
+    String not = "Not";
+    isReady2 = not.equals(not) ? true : false;
+
+        System.out.println(not);
+
+
+    if (isReady) {
+        System.out.println(isReady);
+    }else{
+        System.out.println("Not");
+    }
+*/
+    double zm1 = 20;
+    double zm2 = 40;
+    double suma = zm1 +zm2;
+    double rezultat = suma * 100 %40d;
+
+    boolean spr = rezultat == 0;
+
+
+    System.out.println(spr);
+
+
+
+
+
+
+
+
+
+    //byte, long, short, int
+
+        // write your code here
+    }
+}
